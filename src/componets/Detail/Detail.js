1630a9c6fc05b901402fe0134ec9d94d8e1d54fc
Teshie <teshomeaychiluhem@gmail.com>
2022-10-14 00:30:30 -0700

    pdf download done

--- src/componets/Detail/Detail.js
+++ src/componets/Detail/Detail.js
@@ -1,16 +1,32 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import { userInfo } from "../data/data";
-
+import html2canvas from "html2canvas";
+import { jsPDF } from "jspdf";
 const Detail = () => {
   const [data, setData] = useState(userInfo);
   const { id } = useParams();
   console.log(id);
+  const printRef = useRef();
+
+  const handleDownloadPdf = async () => {
+    const element = printRef.current;
+    const canvas = await html2canvas(element);
+    const data = canvas.toDataURL("image/png");
+
+    const pdf = new jsPDF();
+    const imgProperties = pdf.getImageProperties(data);
+    const pdfWidth = pdf.internal.pageSize.getWidth();
+    const pdfHeight = (imgProperties.height * pdfWidth) / imgProperties.width;
+
+    pdf.addImage(data, "PNG", 0, 0, pdfWidth, pdfHeight);
+    pdf.save("print.pdf");
+  };
 
   return (
     <div className="flex justify-center items-center">
       <div className="h-[30rem] w-[40rem] border">
-        <div>
+        <div ref={printRef}>
           {data
             ?.filter((info) => info.id === id)
             .map((datas) => (
@@ -48,6 +64,9 @@ const Detail = () => {
                           d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
                         />
                       </svg>
+                      <button type="button" onClick={handleDownloadPdf}>
+                        Download as PDF
+                      </button>
                     </div>
                   </div>
                 </div>
